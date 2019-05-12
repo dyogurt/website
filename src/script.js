939bc044d0d7413d899b71bfd883f0d5ffb64b08
Diogo Matias <dyogurt@MacBook-Pro-6.local>
2019-05-12 21:24:43 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,44 @@
+var header = `
+<header class="pv4-ns">
+  <h1><a class="f2-ns f1 lh-title normal no-underline dark-gray" href="#home">greekδiogurt</a></h1>
+  <nav class="db-ns dn">
+    <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="index.html">illustration</a>
+    <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="animation.html">animation</a>
+    <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="me.html">me</a>
+    <a class="no-underline dim f2 lh-copy normal dark-gray" href="resources.html">resources</a>
+  </nav>
+  <!--mobile nav menu-->
+  <nav class="db dn-l dn-m dn-ns">
+    <div class="f2 dim pointer" onclick="open_menu()">menu</div>
+    <div id="mobile_menu_links" class="dn">
+      <div class="flex flex-column pt4">
+        <a class="no-underline dim f2 lh-copy normal dark-gray" href="index.html">illustration</a>
+        <a class="no-underline dim f2 lh-copy normal dark-gray" href="animation.html">animation</a>
+        <a class="no-underline dim f2 lh-copy normal dark-gray" href="me.html">me</a>
+        <a class="no-underline dim f2 lh-copy normal dark-gray" href="resources.html">resources</a>
+      </div>
+    </div>
+  </nav>
+</header>
+`;
+
+var footer = `
+  <footer>Es um pandinho!</footer>
+`
+
+ko.applyBindings({
+  header: ko.observable(header),
+  footer: ko.observable(footer),
+});
+
+function open_menu() {
+  document.
+    getElementById("mobile_menu_links").
+    classList.
+    toggle("dn");
+  // if (x.style.display === "none") {
+  //   x.style.display = "block";
+  // } else {
+  //   x.style.display = "none";
+  // }
+}
