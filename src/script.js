d6bc91738ea78bedc8111f0c0cf34ff8a38bd29c
Diogo Matias <dyogurt@MacBook-Pro-6.local>
2019-05-25 19:12:22 +0100

    fixed link

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const headerHTML = `
 <header class="pv4-ns">
   <h1><a class="f2-ns f1 lh-title normal no-underline dim dark-gray" href="me.html">δyogurt</a></h1>
   <nav class="db-ns dn">
-    <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="index.html">illustration</a>
+    <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="illustration.html">illustration</a>
     <a class="no-underline dim f2 lh-copy normal dark-gray mr4" href="animation.html">animation</a>
     <a class="no-underline dim f2 lh-copy normal dark-gray" href="resources.html">resources</a>
   </nav>
@@ -11,7 +11,7 @@ const headerHTML = `
     <div class="f2 dim pointer" onclick="open_menu()">menu</div>
     <div id="mobile_menu_links" class="dn">
       <div class="flex flex-column pt4">
-        <a class="no-underline dim f2 lh-copy normal dark-gray" href="index.html">illustration</a>
+        <a class="no-underline dim f2 lh-copy normal dark-gray" href="illustration.html">illustration</a>
         <a class="no-underline dim f2 lh-copy normal dark-gray" href="animation.html">animation</a>
         <a class="no-underline dim f2 lh-copy normal dark-gray" href="resources.html">resources</a>
       </div>
